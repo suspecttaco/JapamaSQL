b2de23893ae15e47092fed5490ec12060234a810
Suspect <50780739+suspecttaco@users.noreply.github.com>
2025-04-25 16:12:45 -0700

    Schema Personas listo

---
+++ Personas.sql
@@ -0,0 +1,55 @@
+use BD_JAPAMA
+go
+
+create table [Personas].[Localidad](
+    [LocalidadId] int primary key identity (1,1) not null,
+    [nombre] varchar(50) not null,
+    [fecha_mod] datetime not null,
+)
+
+create table [Personas].[TipoDomicilio] (
+    [TipoDomicilioId] int primary key identity (1,1) not null,
+    [descripcion] varchar(50),
+    [fecha_mod] datetime not null
+)
+
+create table [Personas].[Domicilio](
+    [DomicilioId] bigint primary key identity (1,1) not null,
+    [domicilio_linea1] varchar(100) not null,
+    [domicilio_linea2] varchar(100),
+    [cod_postal] varchar(5),
+    [LocalidadId] int,
+    [TipoDomicilioId] int,
+    [fecha_mod] datetime not null
+)
+
+create table [Personas].[TipoNumero](
+    [TipoNumeroId] int primary key identity (1,1) not null,
+    [descripcion] varchar(50),
+    [fecha_mod] datetime not null
+)
+
+create table [Personas].[NumeroTelefonico] (
+    [NumeroTelId] bigint primary key identity (1,1) not null,
+    [numero] varchar(10),
+    [TipoNumeroId] int foreign key references Personas.TipoNumero([TipoNumeroId]),
+    [fecha_mod] datetime not null
+)
+
+create table [Personas].[Email] (
+    [EmailId] bigint primary key identity (1,1) not null,
+    [correo] varchar(50) unique,
+    [fecha_mod] datetime
+)
+
+create table [Personas].[Persona] (
+    [PersonaId] bigint primary key identity (1,1) not null,
+    [nombre] varchar(50) not null,
+    [apellido1] varchar(50),
+    [apellido2] varchar(50),
+    [tipo_persona] varchar(1) not null,
+    [EmailId] bigint foreign key references Personas.Email([EmailId]),
+    [NumeroTelId] bigint foreign key references Personas.NumeroTelefonico([NumeroTelId]),
+    [DomicilioId] bigint,
+    [fecha_mod] datetime
+)
