a292bbd8a2ca9902bcf90760696ad64b1b8990c1
suspecttaco <eladielgomez@gmail.com>
2025-04-26 01:23:14 -0700

    Schema Relaciones.sql y Sucursales.sql Actualizado

--- Relaciones.sql
+++ Relaciones.sql
@@ -12,5 +12,67 @@ add constraint FK_Taller_Persona
 foreign key (PersonaId)
 references Personas.Persona (PersonaId)
 
+--Inventario a Establecimiento
+alter table Inventarios.Facturacion
+add constraint FK_Facturacion_Establecimiento
+foreign key (EstablecimientoId)
+references Sucursales.Establecimiento (EstablecimientoId)
 
+alter table Inventarios.Vehiculo
+add constraint FK_Vehiculo_Establecimiento
+foreign key (EstablecimientoId)
+references Sucursales.Establecimiento (EstablecimientoId)
 
+--Cliente a Persona
+alter table Clientes.Cliente
+add constraint FK_Cliente_Persona
+foreign key (PersonaId)
+references Personas.Persona (PersonaId)
+
+--Cliente a Sucursal
+alter table Clientes.Adeudos
+add constraint FK_Adeudo_Establecimiento
+foreign key (EstablecimientoId)
+references Sucursales.Establecimiento (EstablecimientoId)
+
+--ReportesServicio a Cliente
+alter table Servicios.ReporteProblema
+add constraint FK_Reporte_Cliente
+foreign key (ClienteId)
+references Clientes.Cliente (ClienteId)
+
+--Suspension a Cliente
+alter table Servicios.SuspensionServicio
+add constraint FK_Suspension_Cliente
+foreign key (ClienteId)
+references Clientes.Cliente (ClienteId)
+
+--Carta a Cliente
+alter table Servicios.CartaNoAdeudos
+add constraint FK_Carta_Cliente
+foreign key (ClienteId)
+references Clientes.Cliente (ClienteId)
+
+--Sucursal a Domicilio
+alter table Sucursales.Establecimiento
+add constraint FK_Establecimiento_Domicilio
+foreign key (DomicilioId)
+references Personas.Domicilio (DomicilioId)
+
+--Red a Proveedor
+alter table Sucursales.Red
+add constraint FK_Red_Proveedor
+foreign key (ProveedorId)
+references Inventarios.Proveedor (ProveedorId)
+
+--Cajero a Banco
+alter table Sucursales.CajeroAutomatico
+add constraint FK_Cajero_Banco
+foreign key (BancoId)
+references RecursosHumanos.Banco (BancoId)
+
+--Empleado a Persona
+alter table RecursosHumanos.Empleado
+add constraint FK_Empleado_Persona
+foreign key (PersonaId)
+references Personas.Persona (PersonaId)
--- Sucursales.sql
+++ Sucursales.sql
@@ -1,46 +1,46 @@
 use BD_JAPAMA
 go
+CREATE TABLE Sucursales.Departamento (
+    DepartamentoId int PRIMARY KEY NOT NULL,
+    CONSTRAINT DepartamentoId UNIQUE(DepartamentoId),
+    descripcion VARCHAR(50) NOT NULL,
+    segmento_red VARCHAR(16) NOT NULL,
+    fecha_mod DATE NOT NULL,
+);
+
+CREATE TABLE Sucursales.Red (
+    RedId INT PRIMARY KEY NOT NULL,
+    CONSTRAINT RedId UNIQUE(RedId),
+    compania_red VARCHAR(50) NOT NULL,
+    segmento_red VARCHAR(16) NOT NULL,
+    ProveedorId INT NOT NULL,
+    fecha_mod DATE NOT NULL,
+);
+
+CREATE TABLE Sucursales.TipoEstablecimiento (
+    TipoEstablecimientId BIGINT PRIMARY KEY NOT NULL,
+    CONSTRAINT TipoEstablecimientoId UNIQUE(TipoEstablecimientId),
+    nombre VARCHAR(50) NOT NULL,
+    fecha_mod DATE NOT NULL,
+);
 
 CREATE TABLE Sucursales.Establecimiento (
     EstablecimientoId BIGINT PRIMARY KEY NOT NULL,
     CONSTRAINT EstablecimientoId UNIQUE(EstablecimientoId),
 
     nombre VARCHAR(50) NOT NULL,
-    TipoEstablecimientoId BIGINT NOT NULL,
+    TipoEstablecimientoId BIGINT NOT NULL FOREIGN KEY REFERENCES Sucursales.TipoEstablecimiento (TipoEstablecimientId),
     DomicilioId BIGINT NOT NULL,
-    RedId INT NOT NULL,
-    DepartamentoId INT NOT NULL,
+    RedId INT NOT NULL FOREIGN KEY REFERENCES Sucursales.Red (RedId),
+    DepartamentoId INT NOT NULL FOREIGN KEY REFERENCES Sucursales.Departamento (DepartamentoId),
     fecha_mod DATE NOT NULL,
 );
-CREATE TABLE Sucursales.Red (
-    RedId INT PRIMARY KEY NOT NULL,
-    CONSTRAINT RedId UNIQUE(RedId),
-    
-    compania_red VARCHAR(50) NOT NULL,
-    segmento_red VARCHAR(16) NOT NULL,
-    ProveedorId BIGINT NOT NULL,
-    fecha_mod DATE NOT NULL,
-);
-CREATE TABLE Sucursales.TipoEstablecimiento (
-    TipoEstablecimientId BIGINT PRIMARY KEY NOT NULL,
-    CONSTRAINT TipoEstablecimientoId UNIQUE(TipoEstablecimientId),
 
-    nombre VARCHAR(50) NOT NULL,
-    fecha_mod DATE NOT NULL,
-);
-CREATE TABLE Sucursales.Departamento (
-    DepartamentoId BIGINT PRIMARY KEY NOT NULL,
-    CONSTRAINT DepartamentoId UNIQUE(DepartamentoId),
-
-    descripcion VARCHAR(50) NOT NULL,
-    segmento_red VARCHAR(16) NOT NULL,
-    fecha_mod DATE NOT NULL,
-);
 CREATE TABLE Sucursales.CajeroAutomatico (
     CajeroId BIGINT PRIMARY KEY NOT NULL,
     CONSTRAINT CajeroId UNIQUE(CajeroId),
 
-    EstablecimientoId BIGINT NOT NULL,
+    EstablecimientoId BIGINT NOT NULL FOREIGN KEY REFERENCES Sucursales.Establecimiento (EstablecimientoId),
     actividad CHAR(1) NOT NULL,
     CONSTRAINT actividad CHECK(actividad IN('A','I','S')), --Activo ->A Inactivo ->I Suspendido ->S
     fecha_inicio DATE NOT NULL,
